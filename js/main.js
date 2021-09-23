1b40bf09a10234bd04fe36de50f08d3c271c2277
nizaguir <nmaijo@icloud.com>
2021-09-22 23:44:26 -0500

    working through the prepare

--- js/main.js
+++ js/main.js
@@ -3,5 +3,15 @@ const links = [
     label: "Week1 notes",
     url: "week1/index.html"
   }
+
+  {
+    label: "Week2 notes",
+    url: "week2/index.html"
+  }
+
+  {
+    label: "Week3 notes",
+    url: "week3/index.html"
+  }
 ]
         
