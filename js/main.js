a708832b0e62f1959bf7e7819475a75b87e65bc3
nizaguir <nmaijo@icloud.com>
2021-09-23 23:09:00 -0500

    story editor

--- js/main.js
+++ js/main.js
@@ -9,3 +9,13 @@ const links = [
   }
 ]
         
+
+let data = ["Ram", "Shyam", "Sita", "Gita"];
+  
+      let list = document.getElementById("myList");
+  
+      data.forEach((item) => {
+        let li = document.createElement("li");
+        li.innerText = item;
+        list.appendChild(li);
+      });
